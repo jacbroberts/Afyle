02bed56c32efdcee0b6df9465c1bdf0c5bee9896
Ubuntu <ubuntu@ip-172-26-15-193.us-east-2.compute.internal>
2022-06-30 20:33:20 +0000

    First production version

---
+++ config/gunicorn/dev.py
@@ -0,0 +1,9 @@
+wsgi_app = "afyle.wsgi:application"
+loglevel = "debug"
+workers = 2
+bind = "0.0.0.0:8000"
+reload = True
+accesslog = errorlog = "/var/log/gunicorn/dev.log"
+capture_output = True
+pidfile = "/var/run/gunicorn/dev.pid"
+daemon = True
---
+++ config/gunicorn/prod.py
@@ -0,0 +1,10 @@
+import multiprocessing
+
+wsgi_app = "afyle.wsgi:application"
+workers = multiprocessing.cpu_count() * 2 + 1
+bind = "0.0.0.0:8000"
+accesslog = "/var/log/gunicorn/access.log"
+errorlog = "/var/log/gunicorn/error.log"
+capture_output = True
+pidfile = "/var/run/gunicorn/prod.pid"
+daemon = True
