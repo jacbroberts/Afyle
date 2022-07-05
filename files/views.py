030ec7dc3fede5e2f1ae4eb119e922a7b4bb49f9
Jacob <jacbroberts@gmail.com>
2022-07-05 11:27:41 -0500

    fix for server error on files and account

--- files/views.py
+++ files/views.py
@@ -45,12 +45,12 @@ def register(request):
 #if logged in
 @login_required
 def files(request):
-    user_storage_data = UserStorageData.get(username__exact=request.user)
+    user_storage_data = UserStorageData.get(username__exact=request.user.get_username)
     return render(request, 'files/files.html', {"userStorage":user_storage_data})
 
 @login_required
 def account(request):
-    user_storage_data = UserStorageData.get(username__exact=request.user)
+    user_storage_data = UserStorageData.get(username__exact=request.user.get_username)
     return render(request, 'files/account.html', {"userStorage":user_storage_data})
 
 def media_access(request, path):
