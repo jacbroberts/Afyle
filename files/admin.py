3f68a12e10a933350935146155e3880a5127d0e5
Jacob <jacbroberts@gmail.com>
2022-07-29 13:57:12 -0500

    groups v0.4.4

--- files/admin.py
+++ files/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import UserStorageData
+from .models import UserStorageData, Party, UserPartyList
 
 # Register your models here.
 
 admin.site.register(UserStorageData)
+admin.site.register(Party)
+admin.site.register(UserPartyList)
