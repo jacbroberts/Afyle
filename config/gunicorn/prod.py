12aa0e5faeb1e05c3a1f26a6ff099baa784220af
AfyleServer <jacbroberts@gmail.com>
2022-07-29 18:55:58 +0000

    Post migrations

--- config/gunicorn/prod.py
+++ config/gunicorn/prod.py
@@ -3,6 +3,7 @@ import multiprocessing
 wsgi_app = "afyle.wsgi:application"
 workers = multiprocessing.cpu_count() * 2 + 1
 bind = "0.0.0.0:8000"
+loglevel = "debug"
 accesslog = "/var/log/gunicorn/access.log"
 errorlog = "/var/log/gunicorn/error.log"
 capture_output = True
