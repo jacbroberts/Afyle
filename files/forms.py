a126c9b42fdfe02cee52e8cba80dc0d822315b75
Jacob <jacbroberts@gmail.com>
2022-07-31 17:39:05 -0500

    kanban v0.3

--- files/forms.py
+++ files/forms.py
@@ -1,6 +1,6 @@
 from django import forms 
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Kanban
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -21,3 +21,35 @@ class NewGroupForm(forms.Form):
 class InviteUserToParty(forms.Form):
     email = forms.EmailField(required=False)
     name = forms.CharField(required=False)
+
+class NewKanbanBoard(forms.Form):
+    title = forms.CharField(required=True, max_length=256)
+    description = forms.CharField(required=False, max_length=1024)
+
+
+
+archive_method_choices = (
+    ("1", "Time after task put into column"),
+    ("2", "Task count"),
+    ("3", "Time after due"),
+)
+
+sort_method_choices = (
+    ("1", "Priority"),
+    ("2", "Due Date"),
+    ("3", "Time Created"),
+    ("4", "Alphabetically"),
+    ("5", "Manual")
+)
+
+class NewKanbanColumn(forms.Form):
+    title = forms.CharField(required=True, max_length=256)
+    archive_method = forms.ChoiceField(choices=archive_method_choices)
+    sort_method = forms.ChoiceField(choices=sort_method_choices)
+
+
+class NewKanbanTask(forms.Form):
+    title = forms.CharField(required=True, max_length=256)
+    description = forms.CharField(required=False, max_length=1024)
+    due_date = forms.DateField()
+    priority = forms.IntegerField(min_value=1, max_value=4, initial=1)
--- files/views.py
+++ files/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
 
 from files.models import UserStorageData, Party, UserPartyList, Notification, Kanban, TaskColumn, Task
-from .forms import NewUserForm, UploadFileForm, NewGroupForm, InviteUserToParty
+from .forms import NewUserForm, UploadFileForm, NewGroupForm, InviteUserToParty, NewKanbanBoard
 from django.contrib.auth.models import User
 
 import datetime
@@ -248,15 +248,33 @@ def notifications(request):
 @login_required
 def kanbans(request, type, owner):
     if request.method == 'POST':
-        pass 
+        form = NewKanbanBoard(request.POST)
+        if form.is_valid():
+            new_title = form.cleaned_data.get("title")
+            
+            if type == "user":
+                if Kanban.objects.filter(type=type).filter(user=request.user).get(title=new_title):
+                    print("already exists")
+                else:
+                    new_kanban = Kanban(type="user", user=request.user, title=new_title, description=form.cleaned_data.get("description"))
+            if type == "group" and is_party_member(request.user, owner):
+                if Kanban.objects.filter(type=type).filter(party=Party.get(name=owner)).get(title=new_title):
+                    print("already exists")
+                else:
+                    new_kanban = Kanban(type="group", party=Party.get(name=owner), title=new_title, description=form.cleaned_data.get("description"))
+                    
+            new_kanban.save()
+            
+
     else:
+        form = NewKanbanBoard()
         if type == "user":
             kanban = Kanban.objects.filter(user=request.user)
         if type == "group" and is_party_member(request.user, owner):
             party = Party.objects.get(name=owner)
             kanban = Kanban.objects.filter(party=party)
         
-    return render(request, 'files/kanbans.html', {"kanbans":kanban})
+    return render(request, 'files/kanbans.html', {"kanbans":kanban, "form":form})
 
 @login_required
 def kanban(request, type, owner, title):
