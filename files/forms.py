3d6bc0257960124a3dadb0a9aba00a52ad4586f6
Jacob <jacbroberts@gmail.com>
2022-07-31 17:40:16 -0500

    kanban v0.3.1

--- files/forms.py
+++ files/forms.py
@@ -1,6 +1,6 @@
 from django import forms 
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User, Kanban
+from django.contrib.auth.models import User
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
