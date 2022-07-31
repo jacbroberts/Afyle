959298e6ab33fbcb8a3b8e67ba838a078d0f231a
Jacob <jacbroberts@gmail.com>
2022-07-31 09:32:23 -0500

    kanban v0.1.8

--- files/admin.py
+++ files/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserStorageData, Party, UserPartyList, Notification, Kanban, TaskColumns, Tasks
+from .models import UserStorageData, Party, UserPartyList, Notification, Kanban, TaskColumn, Task
 
 # Register your models here.
 
@@ -8,5 +8,5 @@ admin.site.register(Party)
 admin.site.register(UserPartyList)
 admin.site.register(Notification)
 admin.site.register(Kanban)
-admin.site.register(TaskColumns)
-admin.site.register(Tasks)
+admin.site.register(TaskColumn)
+admin.site.register(Task)
